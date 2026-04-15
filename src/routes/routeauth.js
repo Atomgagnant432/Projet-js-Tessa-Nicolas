52ff83f6206e3c9b894b9e349bd6ccf84051347a
dtessa <tessa.decung@ynov.com>
2026-04-15 09:01:11 +0200

    feat(Favorite): updated the routes, added favorite ones

--- src/routes/routeauth.js
+++ src/routes/routeauth.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../../controllers/authcontroller');
+const favoritesController = require("../../controllers/favcontroller");
 const isAuth = require('../middlewares/middlewares');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 router.get('/me', isAuth, authController.me);
+
+router.get("/api/favorites", isAuthenticated, favController.getFavorites);
+router.post("/api/favorites/toggle", isAuthenticated, favController.toggleFavorite);
 
 module.exports = router;
