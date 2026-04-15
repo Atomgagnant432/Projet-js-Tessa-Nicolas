030afb02a783c43458860f4ad2954869b2c559d8
dtessa <tessa.decung@ynov.com>
2026-04-15 11:21:27 +0200

    feat(Favorite): added routes

--- src/routes/routeauth.js
+++ src/routes/routeauth.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../../controllers/authcontroller');
-const favoritesController = require("../../controllers/favcontroller");
+const favController = require("../../controllers/favcontroller");
+const movieController = require("../../controllers/moviecontroller");
 const isAuth = require('../middlewares/middlewares');
 
 router.post('/register', authController.register);
@@ -9,7 +10,8 @@ router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 router.get('/me', isAuth, authController.me);
 
-router.get("/api/favorites", isAuthenticated, favController.getFavorites);
-router.post("/api/favorites/toggle", isAuthenticated, favController.toggleFavorite);
+router.get('/tmdb-proxy/:type/:id', isAuth, movieController.getMovieDetails);
+router.get("/favorites", isAuth, favController.getFavorites);
+router.post("/favorites/toggle", isAuth, favController.toggleFavorite);
 
 module.exports = router;
